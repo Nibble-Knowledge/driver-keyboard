e06acc2a2a41ac5cd0764c22f06894fe570ac9f4
Naomi <numberless1@gmail.com>
2016-03-17 14:15:36 -0600

    Updated driver to use chip select address labels, and deselect keyboard when done

--- ps2driver.s
+++ ps2driver.s
@@ -3,14 +3,14 @@
 PS2_DRIVER.Enter:
 
 #Change when final chip select assignments made
-MOV N_[1] INTO CHIP_SELECT
+MOV CS_KEYBOARD INTO CHIP_SELECT
 
 #Check for ready signal - if not given, note this and return.
 #If given, note this and continue
 
 OR STATUS_BUS N_[0b0010] INTO PS2_DRIVER.GotKey
 LOD PS2_DRIVER.GotKey
-JMP PS2_DRIVER.Exit
+JMP PS2_DRIVER.Done
 
 #If a key was found, read it.
 
@@ -23,8 +23,11 @@ MOV DATA_BUS INTO PS2_DRIVER.Keystroke[1]
 MOV N_[0b0000] INTO STATUS_BUS
 
 #Return
+PS2_DRIVER.Done:
 
+MOV N_[F] INTO CHIP_SELECT
 LOD N_[0]
+
 PS2_DRIVER.Exit:
 JMP 0x0000
 
